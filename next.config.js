9ccc0ab18764cf5087fd98529a61e032315939d2
baraah-berra <baraahalsaidramadan@gmail.com>
2025-02-17 19:42:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  typescript: {
+
+    // ignoreBuildErrors: true,
+  }
+,
   typescript: {
     ignoreBuildErrors: true
   },
